4d0bfc2f71d6ce364c736167db1042e28af5e6fd
baseonmars <dan@baseonmars.co.uk>
2012-04-27 02:03:30 +0100

    load jquery plugins with require.js

---
+++ src/main.js
@@ -0,0 +1,11 @@
+define([
+    'order!../lib/class.js',
+    'order!../src/transistor.js',
+    '../lib/md5.js',
+    '../lib/json2.js',
+    '../lib/jquery.url.js',
+    '../lib/jquery.cookie.js'
+],
+function(Class, Transistor) {
+    window.Transistor = Transistor;
+});
