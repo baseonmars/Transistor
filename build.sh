3ac0302cb21655bab66b11b38d8d6383cbff336b
baseonmars <dan@baseonmars.co.uk>
2012-04-27 02:02:57 +0100

    Async version of the library, using almond as a wrapper

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-node ./lib/r.js -o name=src/main out=lib/transistor.js baseUrl=.
+node ./lib/r.js -o name=src/main out=lib/transistor-async.js baseUrl=.
+node ./lib/r.js -o name=lib/almond.js include=src/main out=lib/transistor-sync.js baseUrl=.
--- src/main.js
+++
@@ -1,9 +0,0 @@
-define([
-    'order!../lib/class.js',
-    'order!../src/transistor.js',
-    '../lib/md5.js',
-    '../lib/json2.js'
-],
-function(Class, Transistor) {
-    window.Transistor = Transistor;
-});
