41353b7223e08cd828050ebe592a7cbe6c0ffb06
baseonmars <dan@baseonmars.co.uk>
2012-05-19 23:30:45 +0100

    basic player specs for public methods

--- spec/player.spec.js
+++ spec/player.spec.js
@@ -8,7 +8,7 @@ require.config({
     }
 });
 define([
-    '../src/player',
+    '../src/player', 
     'require', "soundmanager", "depend!amplify[jquery]"], function (Player, require) {
 
         window.soundManager = new SoundManager();
@@ -53,9 +53,64 @@ define([
             player = new Player(window.soundManager);
             waitsFor(function () { return ready; }, "player", 200);
             runs(function () {
-                expect(player.device.enabled).toBe(true);
                 expect(id).toBe(player.id);
             });
         });
+
+        describe("when asked to play a track", function () {
+
+            var location, player, track, startVolume;
+            beforeEach(function () {
+                location = "http://play.last.fm/astreamid";
+                player = new Player(window.soundManager);
+                startVolume = player.volume;
+                track  = {location: location};
+            });
+
+            it("should play a track", function () {
+
+                spyOn(player.device, 'createSound');
+                player.play(track);
+                expect(player.device.createSound).toHaveBeenCalled();
+                expect(deviceArgs(player)[0].url).toBe(location);
+            });
+
+            it("should resume a track", function () {
+                player.audio = {
+                    resume: jasmine.createSpy()
+                };
+                spyOn(player, 'hasTrack').andReturn(true);
+                player.play();
+                expect(player.audio.resume).toHaveBeenCalled();
+            });
+
+            it("should pause a track", function () {
+
+                player.audio = {pause: jasmine.createSpy()};
+                player.pause();
+                expect(player.audio.pause).toHaveBeenCalled();
+            });
+
+            it("should set volume", function () {
+                var lowVolume = 10, midVolume = 50;
+
+                player.setVolume(lowVolume);
+                expect(player.volume).toBe(lowVolume);
+                player.setVolume(midVolume);
+                expect(player.volume).toBe(midVolume);
+            });
+
+            it("should set volume of new track", function () {
+
+                spyOn(player.device, 'createSound');
+                player.setVolume(10);
+                player.play(track);
+                expect(deviceArgs(player)[0].volume).toBe(10);
+            });
+        });
     });
+
+    function deviceArgs(player) {
+        return player.device.createSound.mostRecentCall.args;
+    }
 });
